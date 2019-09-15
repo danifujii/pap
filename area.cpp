f969ccb1a4511057b406043ea9d0a1c96a8e6c16
Daniel Fujii <danifujii@gmail.com>
2019-09-15 15:46:54 -0300

    Well, almost fixed area problem

--- area.cpp
+++ area.cpp
@@ -3,53 +3,58 @@
 
 using namespace std;
 
-typedef pair<int, int> cols;
+typedef long long ll;
 
-pair<int, int> kadane_1d(vector<int> vals, int lim) {
-    int best, curr; best = curr = 0;
+pair<ll, ll> kadane_1d(vector<ll> vals, ll lim, int top, int bottom) {
+    ll best, curr; best = curr = 0;
     int best_length = 0, length = 0;
 
     for (int i = 0; i < vals.size(); i++) {
-        curr = curr + vals[i];
-        if (curr > lim) {
-            if (curr-vals[i] <= lim)
-                best = curr - vals[i]; best_length = length;
-            curr = vals[i];
-            length = 1;
-        } else {
-            best = curr;
-            length += 1;
+        if (length > best_length) {
             best_length = length;
+            best = curr;
+        }
+        curr = curr + vals[i];
+        length += 1;
+        if (curr > lim) {
+            // try fixing it by removing start elements
+            while (curr > lim && length > 0) {
+                auto remove = vals[i-length+1];
+                curr -= remove;
+                length -= 1;
+            }
         }
     }
-    return pair<int, int>(best, best_length);
+    if (length > best_length) {
+        best_length = length;
+        best = curr;
+    }
+    return pair<ll, ll>(best, best_length);
 }
 
 int get_area(int width, int r1, int r2) {
     return width * (r2-r1);
 }
 
-pair<int, int> get_best_area_price(vector<vector<int>> matrix, int lim) {
+pair<ll, ll> get_best_area_price(vector<vector<ll>> matrix, ll lim) {
     for (int c = 0; c < matrix[0].size(); c++)
         for (int r = 0; r < matrix.size(); r++)
             matrix[r][c] = matrix[r][c] + ((r > 0) ? matrix[r-1][c] : 0);
 
     // Add a single row with zeros at the beginning, helps with math
-    vector<int> first;
+    vector<ll> first;
     for (int i = 0; i < matrix[0].size(); i++) first.push_back(0);
     matrix.insert(matrix.begin(), first);
 
-    int best_area = -1, best_price = 100000;
+    ll best_area = -1, best_price = 100000000;
 
     for (int top = 0; top < matrix.size(); top++){
         for (int bottom = top; bottom < matrix.size(); bottom++) {
-            if (top == bottom) continue;
-
-            vector<int> range_sum;
+            vector<ll> range_sum;
             for (int c = 0; c < matrix[0].size(); c++)
                 range_sum.push_back(matrix[bottom][c] - matrix[top][c]);
 
-            auto sol = kadane_1d(range_sum, lim);
+            auto sol = kadane_1d(range_sum, lim, top, bottom);
             auto sol_area = get_area(sol.second, top, bottom);
             if (sol.first <= 0) continue; // no valid area for these rows
 
@@ -62,7 +67,7 @@ pair<int, int> get_best_area_price(vector<vector<int>> matrix, int lim) {
         }
     }
 
-    return pair<int, int>(best_area, best_price);
+    return pair<long, long>(best_area, best_price);
 }
 
 int main(int argc, char const *argv[])
@@ -71,22 +76,23 @@ int main(int argc, char const *argv[])
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int tests, n, m, k, s;
+    ll k;
+    int tests, n, m, s;
     string cas;
 
     cin >> tests;
     for (int t = 0; t < tests; t++) {
         cin >> n >> m >> k;
-        vector<vector<int>> matrix;
+        vector<vector<ll>> matrix;
         for (int ni = 0; ni < n; ni++) {
-            vector<int> row;
+            vector<ll> row;
             for (int mi = 0; mi < m; mi++) {
                 cin >> s;
                 row.push_back(s);
             }
             matrix.push_back(row);
         }
-        pair<int, int> sol = get_best_area_price(matrix, k);
+        pair<ll, ll> sol = get_best_area_price(matrix, k);
         if (sol.first <= 0)
             cout << "Case #" << t+1 << ": " << 0 << " " << 0 << "\n";
         else cout << "Case #" << t+1 << ": " << sol.first << " " << sol.second << "\n";
