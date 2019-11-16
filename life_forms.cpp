bc361326d282697f58b5da1fddf12967fadd25cc
Daniel Fujii <danifujii@gmail.com>
2019-11-16 15:30:20 -0300

    Accepted life forms

--- life_forms.cpp
+++ life_forms.cpp
@@ -4,26 +4,53 @@
 #include <unordered_set>
 #include <unordered_map>
 #include <deque>
+#include <algorithm>
 
 using namespace std;
 
+#define forr(i,a,b) for(int i=(a); i <(b); i++)
+#define forn(i,n) forr(i,0,n)
+#define sz(v) (int)(v).size()
+
 typedef pair<int, int> ii;
+typedef vector<int> vi;
+
+typedef const int&(*op)(const int&, const int&);  // asociativa, idempotente
+template<op op>
+
+struct st{
+    vector<vi> t;
+
+    st(const vi &A) { // O(n.logn)
+        int n = sz(A);
+        int K = 31 - __builtin_clz(n)+1;
+        t.assign(K, vi(n));
+        forn(i,n) t[0][i] = A[i];
+        forr(k,1,K) forn(i, n-(1<<k)+1)
+            t[k][i] = op(t[k-1][i], t[k-1][i+(1<<(k-1))]);
+    }
+
+    int query(int l, int r) {  // O(1), [l, r)
+        int k = 31 - __builtin_clz(r-l);
+        return op(t[k][l], t[k][r-(1<<k)]);
+    }
+};
 
 // Suffix Array O(nlogn)
 // s.push('$');
-vector<int> suffix_array(string &s){
-  int n = s.size(), alph = 256;
-  vector<int> cnt(max(n, alph)), p(n), c(n);
+vi suffix_array(string &s){
+    int n = s.size(), alph = 256;
+    vi cnt(max(n, alph)), p(n), c(n);
 
-  for(auto c : s) cnt[c]++;
-  for(int i = 1; i < alph; i++) cnt[i] += cnt[i - 1];
-  for(int i = 0; i < n; i++) p[--cnt[s[i]]] = i;
-  for(int i = 1; i < n; i++)
+    for(auto c : s) cnt[c]++;
+    for(int i = 1; i < alph; i++) cnt[i] += cnt[i - 1];
+    for(int i = 0; i < n; i++) p[--cnt[s[i]]] = i;
+    for(int i = 1; i < n; i++)
     c[p[i]] = c[p[i - 1]] + (s[p[i]] != s[p[i - 1]]);
 
-  vector<int> c2(n), p2(n);
+    vi c2(n), p2(n);
 
-  for(int k = 0; (1 << k) < n; k++){
+    for(int k = 0; (1 << k) < n; k++){
     int classes = c[p[n - 1]] + 1;
     fill(cnt.begin(), cnt.begin() + classes, 0);
 
@@ -34,32 +61,32 @@ vector<int> suffix_array(string &s){
 
     c2[p[0]] = 0;
     for(int i = 1; i < n; i++){
-      pair<int, int> b1 = {c[p[i]], c[(p[i] + (1 << k))%n]};
-      pair<int, int> b2 = {c[p[i - 1]], c[(p[i - 1] + (1 << k))%n]};
-      c2[p[i]] = c2[p[i - 1]] + (b1 != b2);
+        pair<int, int> b1 = {c[p[i]], c[(p[i] + (1 << k))%n]};
+        pair<int, int> b2 = {c[p[i - 1]], c[(p[i - 1] + (1 << k))%n]};
+        c2[p[i]] = c2[p[i - 1]] + (b1 != b2);
     }
 
     c.swap(c2);
-  }
-  return p;
+    }
+    return p;
 }
 
 // Longest Common Prefix with SA O(n)
-vector<int> lcp(string &s, vector<int> &p){
-  int n = s.size();
-  vector<int> ans(n - 1), pi(n);
-  for(int i = 0; i < n; i++) pi[p[i]] = i;
+vi lcp(string &s, vi &p){
+    int n = s.size();
+    vi ans(n - 1), pi(n);
+    for(int i = 0; i < n; i++) pi[p[i]] = i;
 
-  int lst = 0;
-  for(int i = 0; i < n - 1; i++){
+    int lst = 0;
+    for(int i = 0; i < n - 1; i++){
     if(pi[i] == n - 1) continue;
     while(s[i + lst] == s[p[pi[i] + 1] + lst]) lst++;
 
     ans[pi[i]] = lst;
     lst = max(0, lst - 1);
-  }
+    }
 
-  return ans;
+    return ans;
 }
 
 char get_delimiter(int index) {
@@ -68,7 +95,7 @@ char get_delimiter(int index) {
 }
 
 // get to which string (index) this index belongs to
-int string_belonging(vector<int> & begins, int index) {
+int string_belonging(vi & begins, int index) {
     int left = 0;
     int right = begins.size() - 1;
     while (left <= right) {
@@ -86,79 +113,70 @@ int string_belonging(vector<int> & begins, int index) {
     }
 }
 
+vi get_limits(const vector<string> & ss) {
+   vi begins(ss.size());
+   int previous = 0;
+   for (int i = 0; i < ss.size(); ++i) {
+       begins[i] = previous;
+       previous += ss[i].size() + 1;
+   }
+   return begins;
+}
+
+string get_complete_string(const vector<string> & ss) {
+   string complete;
+   for (int i = 0; i < ss.size(); ++i) {
+       complete += ss[i] + get_delimiter(i);
+   }
+   return complete;
+}
+
 void solve2(const vector<string> & ss) {
     if (ss.size() == 1) { cout << ss[0] << "\n"; return; }
 
-    string complete;
-    vector<int> begins(ss.size());
-    int previous = 0;
-    for (int i = 0; i < ss.size(); ++i) {
-        begins[i] = previous;
-        complete += ss[i] + get_delimiter(i);
-        previous += ss[i].size() + 1;
-    }
-    vector<int> sa = suffix_array(complete); vector<int> ss_lcp = lcp(complete, sa);
-    int bound = ss.size() / 2;
+    // Init data structures
+    string complete = get_complete_string(ss);
+    vi begins = get_limits(ss); vi sa = suffix_array(complete); vi ss_lcp = lcp(complete, sa);
+    st<min> rmq(ss_lcp);
+    int bound = ss.size() / 2 + 1, l = 0, r = 0, max_len = 0;
+    unordered_map<int, int> strings; set<int> lcp_idx;
 
-    unordered_map<int, int> strings; unordered_set<int> max_pre_idxs; deque<ii> queue;
-    int l = 0, r = 0, max = 0;
+    int pre1_str = string_belonging(begins, sa[0]); strings[pre1_str] += 1;
+
     while (r < ss_lcp.size()) {
         int pre1_str = string_belonging(begins, sa[r]);
         int pre2_str = string_belonging(begins, sa[r+1]);
 
-        // If new item would increase the amount of strings, we have remove from left of queue
-        if (strings.find(string_belonging(begins, sa[r+1])) == strings.end()) {
-            while (l <= r && strings.size() == bound+1) {
-                ii q_front = queue.front();
-                if (q_front.second == l) {
-                    if (max == q_front.first) max_pre_idxs.insert(q_front.second);
-                    else if (max < q_front.first) {
-                        max_pre_idxs.clear(); max_pre_idxs.insert(q_front.second); max = q_front.first;
-                    }
-                    queue.pop_front();
+        strings[pre2_str] += 1;
+        // Added new element, reduce it to bound size or
+        // move left to the smallest possible window having bound elements
+        while (l < r && strings.size() >= bound) {
+            int l_str = string_belonging(begins, sa[l]);
+            if (strings.size() == bound && strings[l_str] == 1) break;  // dont break the bound limit
+            strings[l_str] -= 1;
+            if (strings[l_str] == 0) strings.erase(l_str);
+            l += 1;
+
+            if (strings.size() == bound) {
+                int window_min = rmq.query(l, r+1);
+                if (window_min > 0 && window_min >= max_len) {
+                    if (window_min > max_len)
+                        lcp_idx.clear();
+                    lcp_idx.insert(r);
+                    max_len = window_min;
                 }
-                int pre_str = string_belonging(begins, sa[l]);
-                strings[pre_str] -= 1;
-                if (strings[pre_str] == 0) strings.erase(pre_str);
-                l++;
             }
         }
+        r += 1;
+   }
 
-        if (strings.find(pre1_str) != strings.end()) strings[pre1_str] += 1;
-        else strings[pre1_str] = 1;
-        if (strings.find(pre2_str) != strings.end()) strings[pre2_str] += 1;
-        else strings[pre2_str] = 1;
-
-        // Update from the rear with new value in case its lower
-        while (!queue.empty() && queue.back().first > ss_lcp[r]) {
-            ii q_back = queue.back();
-            queue.pop_back();
-            if (strings.size() < bound+1) continue;
-            if (max == q_back.first) max_pre_idxs.insert(q_back.second);
-            else if (max < q_back.first) {
-                max_pre_idxs.clear(); max_pre_idxs.insert(q_back.second); max = q_back.first;
-            }
-        }
-        if (pre1_str != pre2_str) {  // Im not comparing prefixes of the same str
-            if (queue.empty()) {
-                queue.push_back(ii(ss_lcp[r], r));
-            } else queue.push_back(ii(ss_lcp[r], r));
-        }
-
-        r++;
-    }
-    if (strings.size() >= bound+1) {
-        for (ii d: queue) {
-            if (max == d.first) max_pre_idxs.insert(d.second);
-            else if (max < d.first) {  // we found a new maximum
-                max_pre_idxs.clear(); max_pre_idxs.insert(d.second); max = d.first;
-            }
-        }
-    }
-    if (max == 0) cout << "?\n";
-    else {
+    if (max_len == 0) {
+        cout << "?\n";
+    } else {
         set<string> result;
-        for (int index: max_pre_idxs) result.insert(complete.substr(sa[index], ss_lcp[index]));
+        for (int i: lcp_idx) {
+            result.insert(complete.substr(sa[i], max_len));
+        }
         for (string s: result) cout << s << "\n";
     }
 }
